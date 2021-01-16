472171d625b3c51d458613d828a34be0ba54e9ef
Kelvin Nguyen <nhnhatquan@gmail.com>
2021-01-17 01:38:40 +0800

    Update Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,18 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.@(tsx)'],
-  webpackFinal: async config => {
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|svg)$/i,
-      use: {
-        loader: 'base64-inline-loader',
-      },
-    })
-
-    return config
-  },
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 }
+
+// module.exports = {
+//   stories: ['../stories/**/*.stories.@(tsx)'],
+//   webpackFinal: async config => {
+//     config.module.rules.push({
+//       test: /\.(png|jpe?g|gif|svg)$/i,
+//       use: {
+//         loader: 'base64-inline-loader',
+//       },
+//     })
+
+//     return config
+//   },
+// }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,16 @@
-import React from 'react'
+// import React from 'react'
 
-export const decorators = [
-  Story => (
-    <div>
-      <script src="preact-text.js" />
-      <script src="react-text.js" />
-      <Story />
-    </div>
-  ),
-]
+// export const decorators = [
+//   Story => (
+//     <div>
+//       <script src="preact-text.js" />
+//       <script src="react-text.js" />
+//       <Story />
+//     </div>
+//   ),
+// ]
+
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
